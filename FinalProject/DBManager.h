a7e8508d9f3bde83bdbb1e26e1c179863b8a4671
Vidhi Harikrishna Bhatt <bhatt.v@d25rr0qtgg7v.local>
2016-12-04 18:02:13 -0500

    dbmanager

---
+++ FinalProject/DBManager.h
@@ -0,0 +1,25 @@
+//
+//  DBManager.h
+//  FinalProject
+//
+//  Created by Vidhi Harikrishna Bhatt on 12/4/16.
+//  Copyright © 2016 Vidhi Harikrishna Bhatt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+
+@property (nonatomic) int affectedRows;
+
+@property (nonatomic) long long lastInsertedRowID;
+
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+
+-(NSArray *)loadDataFromDB:(NSString *)query;
+
+-(void)executeQuery:(NSString *)query;
+
+@end
