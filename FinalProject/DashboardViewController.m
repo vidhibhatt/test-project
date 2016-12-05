385202b0ce869d532018e602f4e4e06e3f9ad165
Vidhi Harikrishna Bhatt <bhatt.v@d25rr0qfgg7v.local>
2016-12-05 12:13:37 -0500

    database test in dashboard view

--- FinalProject/DashboardViewController.m
+++ FinalProject/DashboardViewController.m
@@ -9,8 +9,15 @@
 #import "DashboardViewController.h"
 #import "SWRevealViewController.h"
 #import "WaterIntakeByDate.h"
+#import "DBManager.h"
 
 @interface DashboardViewController ()
+
+@property (nonatomic, strong) DBManager *dbManager;
+
+@property (nonatomic, strong) NSArray *arrWaterInfo;
+
+-(void)loadData;
 
 @end
 
@@ -35,6 +42,31 @@
     currentDate = today;
     
     todayIntake = [[WaterIntakeByDate alloc] initWithGoal:8 andIntake:0 andProgress:0 andTodayDate: today];
+    
+    // Initialize the dbManager object.
+    self.dbManager = [[DBManager alloc] initWithDatabaseFilename:@"sampledb.sql"];
+    
+    [self loadData ];
+    
+    /*
+     // Prepare the query string.
+     NSString *query = [NSString stringWithFormat:@"insert into waterIntake values('%@', '%d', '%d', %f)", today, 1, 8, 0.0f];
+     
+     // Execute the query.
+     [self.dbManager executeQuery:query];
+     
+     // If the query was successfully executed then pop the view controller.
+     if (self.dbManager.affectedRows != 0) {
+     NSLog(@"Query was executed successfully. Affected rows = %d", self.dbManager.affectedRows);
+     
+     // Pop the view controller.
+     [self.navigationController popViewControllerAnimated:YES];
+     }
+     else{
+     NSLog(@"Could not execute the query.");
+     } */
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,14 +75,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 - (IBAction)addPressed:(UIButton *)sender {
     [todayIntake addIntake];
     _intakeLabel.text = [NSString stringWithFormat: @"%d", todayIntake.intake];
@@ -61,7 +93,7 @@
     [todayIntake removeIntake];
     _intakeLabel.text = [NSString stringWithFormat: @"%d", todayIntake.intake];
     _successLabel.text = [NSString stringWithFormat: @"%f", todayIntake.progress];
-
+    
 }
 
 
@@ -77,6 +109,24 @@
     _currentDateLabel.text = dateString;
     // change current date
     currentDate = todayIntake.currentDate;
+    
+    
+    NSString *query = [NSString stringWithFormat:@"insert into waterIntake values('%@', '%d', '%d', %f)", currentDate, 1, 8, 0.0f];
+    
+    // Execute the query.
+    [self.dbManager executeQuery:query];
+    
+    // If the query was successfully executed then pop the view controller.
+    if (self.dbManager.affectedRows != 0) {
+        NSLog(@"Query was executed successfully. Affected rows = %d", self.dbManager.affectedRows);
+        
+        // Pop the view controller.
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    else{
+        NSLog(@"Could not execute the query.");
+    }
+    
 }
 
 - (IBAction)prevDatePressed:(UIButton *)sender {
@@ -91,6 +141,20 @@
     _currentDateLabel.text = dateString;
     // change current date
     currentDate = todayIntake.currentDate;
+}
+
+-(void)loadData{
+    // Form the query.
+    NSString *query = @"select * from waterIntake";
+    
+    // Get the results.
+    if (self.arrWaterInfo != nil) {
+        self.arrWaterInfo = nil;
+    }
+    self.arrWaterInfo = [[NSArray alloc] initWithArray:[self.dbManager loadDataFromDB:query]];
+    
+    // Reload the table view.
+    //[self.tblPeople reloadData];
 }
 
 
